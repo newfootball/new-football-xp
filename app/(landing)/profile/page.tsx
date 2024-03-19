fae29c77c32dc259623fc504c69b0fa58f2a3299
Fahari <mkl.devops@gmail.com>
2024-03-19 16:34:10 +0000

    feat: #12 add profile page

---
+++ app/(landing)/profile/page.tsx
@@ -0,0 +1,30 @@
+import { auth } from "@/auth/auth";
+import { Container } from "@/components/container";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default async function ProfilePage() {
+  const session = await auth();
+
+  if (!session?.user) {
+    return notFound();
+  }
+
+  return (
+    <Container>
+      <div className="flex items-center gap-4">
+        {session.user.image ? (
+          <Image
+            className="rounded-full"
+            src={session.user.image}
+            alt={`${session.user.name ?? "-"}'s profile picture`}
+            width={96}
+            height={96}
+          />
+        ) : null}
+      </div>
+
+      {JSON.stringify(session.user)}
+    </Container>
+  );
+}
