31e1d4086d2b3c5108a4308e9faae8a65e277488
newfootball <163778229+newfootball@users.noreply.github.com>
2024-03-21 17:36:02 +0000

    10 build  implement pwa (#17)
    Co-authored-by: Fahari <mkl.devops@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -6,4 +6,18 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+export default withPWA(nextConfig);
