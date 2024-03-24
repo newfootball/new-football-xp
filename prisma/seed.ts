e10d977e747ef7d54600ace9c76198fc2798d6b9
MKL DEVOPS <88685078+mkldevops@users.noreply.github.com>
2024-03-24 14:59:32 +0100

    test: #42 prisma  add seed data posts (#44)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { seedUsers } from "./seeds/users";
+import { seedPosts } from "./seeds/posts";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
   const users = await seedUsers(prisma);
+  const posts = await seedPosts({ prisma, users });
 };
 
 main()
